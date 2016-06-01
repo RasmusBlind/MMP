31729eb781f1dda3512e8e65a9dbc33d2ba8caaf
Simone Vinkel <svinke12@student.aau.dk>
2016-06-01 13:14:11 +0200

    Level done
    Har lavet et level, og prøvet med at tilføje random coins på én lane

---
+++ Assets/Scripts/Cameramov.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cameramov : MonoBehaviour {
+
+    public Vector3 myPos;
+    public Transform myPlay;
+     
+    void Update()
+    {
+        transform.position = myPlay.position + myPos;
+    }
+
+}
---
+++ Assets/Scripts/RoadManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadManager : MonoBehaviour {
+
+
+	public GameObject roadPrefab;
+	public GameObject currentRoad;
+    private static RoadManager instance;
+
+    public static RoadManager Instance
+    {
+        get
+        {//If it doesn't exist yet
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<RoadManager>();
+            }
+            return RoadManager.instance;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        //try to spawn it 10 times
+        for(int i = 0; i < 10; i++) { 
+        Spawn();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Spawn (){
+        //Make sure the new spawned road becomes the current one, and cast it as a gameobject
+       currentRoad = (GameObject)Instantiate(roadPrefab, currentRoad.transform.GetChild(0).position, Quaternion.identity);
+
+      int spawnCoin = Random.Range(0, 10);
+
+        if (spawnCoin == 0)
+        {
+            currentRoad.transform.Find("Road/lanepos4/Coin").gameObject.SetActive(true);
+        }
+
+    }
+}
